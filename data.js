e6c2ef6314a4aeff9796691927994fb2eb68fed7
Jbassard <jonathanacciarito@gmail.com>
2024-03-14 19:40:17 -0400

    created rough draft, will just need to update data.js for each application and the rest is good

---
+++ data.js
@@ -0,0 +1,20 @@
+let appHistory = [
+  {
+    date: "date",
+    company: "company",
+    positon: "position",
+    hiringPlatform: "platform",
+    status: "status",
+    coldEmailed: false,
+  },
+  {
+    date: "date",
+    company: "company",
+    positon: "position",
+    hiringPlatform: "platform",
+    status: "status",
+    coldEmailed: false,
+  },
+];
+
+export { appHistory };
---
+++ script.js
@@ -0,0 +1,33 @@
+import { appHistory } from "./data.js";
+
+window.addEventListener("DOMContentLoaded", function () {
+  if (!appHistory) {
+    console.log("couldn't get appHistory");
+    return;
+  }
+
+  const dataContainer = document.getElementById("dataContainer");
+
+  for (const issue of appHistory) {
+    const issueDiv = document.createElement("div");
+
+    issueDiv.innerHTML = `
+        <div class="issue">
+        <p class="label"><b>Date:</b></p>
+        ${issue.date} 
+        <p class="label"><b>Company:</b></p>
+        ${issue.company} 
+        <p class="label"><b>Position:</b></p>
+        ${issue.positon} 
+        <p class="label"><b>Status:</b></p>
+        ${issue.status} 
+        <p class="label"><b>Platform:</b></p>
+        ${issue.hiringPlatform} 
+        <p class="label"><b>Cold Email:</b></p>
+        ${issue.coldEmailed} 
+        </div>
+        `;
+
+    dataContainer.appendChild(issueDiv);
+  }
+});
